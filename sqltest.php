57c7c3971dccaf11acde9e4d860995503cf4af5d
Stephen Tang <sytang@cs.unc.edu>
2017-02-21 17:32:24 -0500

    Fixed typo in sqltest.php

--- sqltest.php
+++ sqltest.php
@@ -1,5 +1,5 @@
 <?php
-$dbhost = getenv("MYSQL_SERVIC_HOST");
+$dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
