4e97bbf2555af09c2e96ea07a186229a657916ef
Stephen Tang <sytang@cs.unc.edu>
2017-02-21 19:00:11 -0500

    Added basic html form

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
+<?php
+	session_start();
+	if ($_POST["usrName"]){
+		$_SESSION["user"] = $_POST["usrName"];
+	}
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>PHP Test</title>
 </head>
 <body>
-<?php echo "<p>Hello, World</p>"; ?>
+<?php
+	if ($_SESSION["user"]){
+		echo "<b>Welcome " . $_SESSION[user] . "</b><br>\n";
+	}
+	else{
+		echo '<form action="index.php" method="post">\n';
+		echo '<input type="text" name="usrName">\n';
+		echo '<input type="submit" value="SUBMITION">\n';
+	}
+?>
 </body>
 </html>
