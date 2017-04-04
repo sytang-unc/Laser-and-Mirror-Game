b4abeeccb8fb76d0f91434f4bf13f96abaeaae1e
Evan Teng <Evan Teng>
2017-04-03 20:56:23 -0400

    Board edits

--- puzzleGenerator.js
+++ puzzleGenerator.js
@@ -1,5 +1,5 @@
 var puzzleGenerator = function(){
-	this.gridSize = 8;
+	this.gridSize = 10;
 	this.decoyCount = 0;
 	this.level = 0;
 	this.times = new Array();
@@ -15,17 +15,17 @@ var puzzleGenerator = function(){
 	this.getPuzzle = function(correct = -1, time = -1){
 		if (correct == -1 && time == -1){
 			return new puzzle(this.gridSize, this.level);
-		}	
-		this.gridSize = init_gridSize != -1? 
-			Math.floor(Math.random()*this.gridSizeInterval 
+		}
+		this.gridSize = init_gridSize != -1?
+			Math.floor(Math.random()*this.gridSizeInterval
 				+ this.gridSizeFloor):
 			init_gridSize;
 		this.decoyCount = init_decoy != -1?
-			Math.floor(Math.random()*this.decoyInterval 
+			Math.floor(Math.random()*this.decoyInterval
 				+ this.decoyFloor):
 			init_decoy;
 		this.level = init_level != -1?
-			Math.floor(Math.random()*this.levelInterval 
+			Math.floor(Math.random()*this.levelInterval
 				+ this.levelFloor):
 			init_level;
 		return new puzzle(this.gridSize, this.level, this.decoyCount);
---
+++ singleboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var puzz = new puzzle(10, 4, 5, 5);
+  puzz.createPathBumpers();
+  puzz.decoyBumpers();
+  puzz.createHiddenAndClues();
+
+  var construct = new boardconstruction();
+
+  $('div table.board').html(construct.generate_table(10));
+  construct.mapDrawHandler(puzz);
+});
