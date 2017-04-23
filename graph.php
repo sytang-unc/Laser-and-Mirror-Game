a4d8169eaba9bda4e61b7791e7118e76fcb1a131
Joshua <nguyeneer@gmail.com>
2017-04-23 15:41:33 -0400

    graph push

--- graph.php
+++ graph.php
@@ -1,30 +1,42 @@
 <?php
 include("phpgraphlib.php");
-    
+
+session_start();
+$host = getenv("MYSQL_SERVICE_HOST");
+$use = getenv("MYSQL_USER");
+$pw = getenv("MYSQL_PASSWORD");
+$db = getenv("MYSQL_DATABASE");
+$user = $_SESSION['user'] 
+
 $graph=new PHPGraphLib(550,350); 
-$link = mysql_connect($host, $use, $pw)
+$link = mysql_connect($host, $db, $pw)
    or die('Could not connect: ' . mysql_error());
      
-mysql_select_db('SCORES') or die('Could not select database');
+mysql_select_db('scores') or die('Could not select database');
   
 $dataArray=array();
   
 //get data from database
-$sql="SELECT score, COUNT(*) AS 'count' FROM SCORES ";
+$sql="SELECT date, score,user FROM scores WHERE user='$user' ORDER BY date";
 $result = mysql_query($sql) or die('Query failed: ' . mysql_error());
 if ($result) {
   while ($row = mysql_fetch_assoc($result)) {
-      $score=$row["score"];
-      $count=$row["count"];
+      $score=$row["date"];
+      $count=$row["score"];
       //add to data areray
       $dataArray[$score]=$count;
   }
 }
   
 //configure graph
+$graph->setDataValues(true);
+$graph->setXValuesHorizontal(true);
 $graph->addData($dataArray);
 $graph->setTitle("Score");
-$graph->setGradient("lime", "green");
-$graph->setBarOutlineColor("black");
+$graph->setBars(false);
+$graph->setDataPoints(true);
+$graph->setDataPointColor('red');
+$graph->setLine(true);
+$graph->setLineColor('red');
 $graph->createGraph();
 ?>
