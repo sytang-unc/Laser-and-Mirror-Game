ec087a1e20bead1854783946668b1e8a0a599460
Andy Ngo <andn03.an@gmail.com>
2017-04-26 03:37:37 -0400

    Account_login.php attempt, Andy N

--- account_login.php
+++ account_login.php
@@ -2,7 +2,39 @@
     //due to information by you all,  Planned simply to use db-connect to access the mysql pod,
     // then based on which button pressed [Register] [login], the conditions should occur.
 	session_start();
-	require('db-connect.php');
+	//require('db-connect.php');
+	$host_route = getenv("MYSQL_SERVICE_HOST");
+	$use = getenv("MYSQL_USER");
+	$pw = getenv("MYSQL_PASSWORD");
+	$db = getenv("MYSQL_DATABASE");
+	$user = $_SESSION['user']; 
+	$conn = new mysql('host_route', 'use', 'pw');
+	if (!$conn){
+	    die("Database Connection Failed" . mysqli_error($conn));
+	}
+   //modify to test for a existing db, this selects the database of your target
+	$select_db = mysqli_select_db($conn, 'ACCOUNTS');
+	if (!$select_db){
+ 	   die("Database Selection Failed" . mysqli_error($conn));
+	}
+	echo //'<div class="container">
+      '<fieldset class="form-signin" method="POST">'
+       . '<h2 class="form-signin-heading" name="labelUser">Login</h2>'
+        .'<div class="user-input">'
+        .'<label for="userName" class="userN">Username</label>'
+        .'<input type="username" name="username" id="inputUserName" class="form-control" placeholder="Username" required autofocus>'
+        .'</div>'
+        .'<div class="passw-input">'
+        .'<label for="inputPassword" class="sr-only">Password</label>'
+        .'<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>'
+        .'</div>'
+        .'<br>'
+        .'<button class="btn btn-lg btn-primary btn-block" name="reg" type="submit">Register</button>'
+        .'<button class="btn btn-lg btn-primary btn-block" name="log" type="submit">Login</button>'
+        .'<button class="btn btn-lg btn-primary btn-block" name="logot" type="submit">Logout</button>'
+      .'</fieldset>';
+//</div>
+    // Registration
 	$username = "";
 	$password = "";
     // If the values are posted, insert them into the database.
@@ -10,12 +42,13 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
  
-        $query = "INSERT INTO ACCOUNTS (username, password) VALUES ('$username', '$password')";
-        $input = mysqli_query($conn, $query);
+        $query = "INSERT INTO ACCOUNTS (username, password) VALUES ($username, $password)";
+        $input = $conn->query($query);
         if($input){
             echo "New User Created Successfully";
+            $user=$username; // set session to username
         }else{
-            echo "User Registration Failed";
+            echo "User Registration Failed!";
         }
     }
     //was planning on using href="db-login.php" for login to pull up a page for the ACCOUNT screen creation foreground.... 4/15/17
@@ -32,6 +65,7 @@
         	$_SESSION['username']= $row['username'];
         	echo "Account Exists, Successfully login as: ";
         	echo $username;
+        	$user=$username;
         } else {
         	echo "Account does not exist, redo your username and/ password please";
         }
@@ -45,28 +79,3 @@
     }
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mirror---AccountLogin</title>
-</head>
-<body>
-<div class="container">
-      <form class="form-signin" method="POST">
-        <h2 class="form-signin-heading" name="labelUser">Login</h2>
-        <div class="user-input">
-        <label for="userName" class="userN">Username</label>
-        <input type="username" name="username" id="inputUserName" class="form-control" placeholder="Username" required autofocus>
-        </div>
-        <div class="passw-input">
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        </div>
-        <br>
-        <button class="btn btn-lg btn-primary btn-block" name="reg" type="submit">Register</button>
-        <button class="btn btn-lg btn-primary btn-block" name="log" type="submit">Login</button>
-        <button class="btn btn-lg btn-primary btn-block" name="logot" type="submit">Logout</button>
-      </form>
-</div>
-</body>
-</html>
