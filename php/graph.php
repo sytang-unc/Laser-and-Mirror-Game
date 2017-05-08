0aa6daf131fdfcbe035db101fcd55e9b80654b48
Stephen Tang <sytang@cs.unc.edu>
2017-05-08 00:35:26 -0400

    Trying to figure out why this friggen graph doesnt show up

--- php/graph.php
+++ php/graph.php
@@ -8,43 +8,43 @@ $pw = getenv("MYSQL_PASSWORD");
 $db = getenv("MYSQL_DATABASE");
 
 
+$conn = new mysqli($host, $use, $pw, $db);
+	
 $graph=new PHPGraphLib(550,350);
 $dataArray=array();
 
-if (isset($_SESSION["user"])){
-
-	$conn = new mysqli($host, $use, $pw, $db);
-		   //or die('Could not connect: ' . mysql_error());
-
-	//mysql_select_db('mirror_storage') or die('Could not select database');
+//mysql_select_db('mirror_storage') or die('Could not select database');
 
 
-	//get data from database
-	$sql="SELECT time, score,username FROM SCORES WHERE username='" . $_SESSION["user"] . "' ORDER BY time";
-	$result = $conn->query($sql);// or die('Query failed: ' . mysql_error());
-	if ($result) {
-
-		while ($row = $result->fetch_array()) {
-			//console.log("FETCHING :\)");
-			$time=$row["time"];
-			$score=$row["score"];
-			//add to data areray
-			$dataArray[$time]=$score;
-		}
+//get data from database
+$sql="SELECT time, score, username FROM SCORES WHERE username='" . $_SESSION["user"] . "' ORDER BY time";
+$result = $conn->query($sql);// or die('Query failed: ' . mysql_error());
+if ($result) {
+	while ($row = $result->fetch_array()) {
+		//console.log("FETCHING :\)");
+		$time=$row["time"];
+		$score=$row["score"];
+		//add to data areray
+		$dataArray[$time]=$score;
 	}
-
-	$conn->close();
+	//configure graph
+	$graph->setDataValues(true);
+	$graph->setXValuesHorizontal(true);
+	$graph->addData($dataArray);
+	$graph->setTitle("Score");
+	$graph->setBars(false);
+	$graph->setDataPoints(true);
+	$graph->setDataPointColor('red');
+	$graph->setLine(true);
+	$graph->setLineColor('red');
+	$graph->createGraph();
 }
-//configure graph
-$graph->setDataValues(true);
-$graph->setXValuesHorizontal(true);
-$graph->addData($dataArray);
-$graph->setTitle("Score");
-$graph->setBars(false);
-$graph->setDataPoints(true);
-$graph->setDataPointColor('red');
-$graph->setLine(true);
-$graph->setLineColor('red');
-$graph->createGraph();
+else{
+	header('Content type:image/jpeg');
+	echo file_get_contents('../assets/nope.jpg');
+}
+
+$conn->close();
+
 
 ?>
